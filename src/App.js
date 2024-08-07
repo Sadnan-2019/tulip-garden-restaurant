d43c24fdd5855939b25d76458db483009b883de1
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2024-08-07 08:03:02 +0600

    landind

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav/Nav';
 import Landing from './components/Landing/Landing';
+import Foods from './components/Foods/Foods';
 
 function App() {
   return (
     <div className=" ">
           <Nav></Nav>
         <Landing></Landing>
+        <Foods></Foods>
     </div>
   );
 }
