3fc90f388fd0ddf8bb5a3273e519169c6f1ca325
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2024-08-09 23:40:05 +0600

    food

--- src/components/Foods/Foods.js
+++ src/components/Foods/Foods.js
@@ -1,120 +1,114 @@
-import React, { useEffect, useState } from 'react';
-import food from "../../assets/food11.png"
-import food1 from "../../assets/food3.png"
-import food2 from "../../assets/tgrone.png"
-import food3 from "../../assets/food4.png"
+import React, { useEffect, useState } from "react";
+import food from "../../assets/food11.png";
+import food1 from "../../assets/food3.png";
+import food2 from "../../assets/tgrone.png";
+import food3 from "../../assets/food4.png";
 // import food4 from "../../assets/food4.png"
-import food5 from "../../assets/food5.png"
-import food6 from "../../assets/food6.png"
-import food7 from "../../assets/food9.png"
-import food8 from "../../assets/food10.png"
-import food9 from "../../assets/food12.png"
-import "./Foods.css"
-import useFetch from '../hooks/useFetch';
+import food5 from "../../assets/food5.png";
+import food6 from "../../assets/food6.png";
+import food7 from "../../assets/food9.png";
+import food8 from "../../assets/food10.png";
+import food9 from "../../assets/food12.png";
+import "./Foods.css";
+import useFetch from "../hooks/useFetch";
 const Foods = () => {
+  return (
+    <div>
+      <div className=" ">
+        <div class="   ">
+          {/* food Menu tab  */}
 
-
-     return (
-          <div>
-               <div className=' '>
-        
-
-
-
-
-            <div class="   ">
-              {/* food Menu tab  */}
-      
-            <div class="partners-intro--thumb aos-init aos-animate   sm:grid-cols-1 lg:grid-cols-1 w-full bg-gradient-to-r from-[#017445] via-[#160a2c] to-[#EF932A]  ">
-              <div class="partners-intro--partners">
-                <div class="partners-intro--partners-item -pos-1">
-                  <img
-                    class="lazy loaded "
-                    data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/icons/partner-icon-1.png"
-                    alt=""
-                    src={food2}
-                    data-was-processed="true"
-                  />
-                </div>
-                <div class="partners-intro--partners-item -pos-2">
-                  <img
-                    class="lazy loaded"
-                    data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/icons/partner-icon-2.png"
-                    alt=""
-                    src={food3}
-                    data-was-processed="true"
-                  />
-                </div>
-                <div class="partners-intro--partners-item -pos-3">
-                  <img
-                    class="lazy loaded"
-                    data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/icons/partner-icon-3.png"
-                    alt=""
-                    src={food9}
-                    data-was-processed="true"
-                  />
-                </div>
-                <div class="partners-intro--partners-item -pos-4">
-                  <img
-                    class="lazy loaded"
-                    data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/icons/partner-icon-4.png"
-                    alt=""
-                    src={food5}
-                    data-was-processed="true"
-                  />
-                </div>
-                <div class="partners-intro--partners-item -pos-5">
-                  <img
-                    class="lazy loaded"
-                    data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/icons/partner-icon-5.png"
-                    alt=""
-                    src={food6}
-                    data-was-processed="true"
-                  />
-                </div>
-                <div class="partners-intro--partners-item -pos-6">
-                  <img
-                    class="lazy loaded"
-                    data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/icons/partner-icon-6.png"
-                    alt=""
-                    src={food}
-                    data-was-processed="true"
-                  />
-                </div>
-                <div class="partners-intro--partners-item -pos-7">
-                  <img
-                    class="lazy loaded"
-                    data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/icons/partner-icon-7.png"
-                    alt=""
-                    src={food7}
-                    data-was-processed="true"
-                  />
-                </div>
-                <div class="partners-intro--partners-item -pos-8">
-                  <img
-                    class="lazy loaded"
-                    data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/icons/partner-icon-8.png"
-                    alt=""
-                    src={food8}
-                    data-was-processed="true"
-                  />
-                </div>
+          <div class="partners-intro--thumb aos-init aos-animate   sm:grid-cols-1 lg:grid-cols-2 w-full bg-gradient-to-r from-[#017445] via-[#160a2c] to-[#EF932A]  ">
+             
+         
+            <div class="partners-intro--partners">
+              <div class="partners-intro--partners-item -pos-1">
+                <img
+                  class="lazy loaded "
+                  data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/icons/partner-icon-1.png"
+                  alt=""
+                  src={food2}
+                  data-was-processed="true"
+                />
               </div>
-              <div class="partners-intro--logo">
+              <div class="partners-intro--partners-item -pos-2">
                 <img
                   class="lazy loaded"
-                  data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/logo-vector.svg"
+                  data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/icons/partner-icon-2.png"
                   alt=""
-                  src={food1}
+                  src={food3}
+                  data-was-processed="true"
+                />
+              </div>
+              <div class="partners-intro--partners-item -pos-3">
+                <img
+                  class="lazy loaded"
+                  data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/icons/partner-icon-3.png"
+                  alt=""
+                  src={food9}
+                  data-was-processed="true"
+                />
+              </div>
+              <div class="partners-intro--partners-item -pos-4">
+                <img
+                  class="lazy loaded"
+                  data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/icons/partner-icon-4.png"
+                  alt=""
+                  src={food5}
+                  data-was-processed="true"
+                />
+              </div>
+              <div class="partners-intro--partners-item -pos-5">
+                <img
+                  class="lazy loaded"
+                  data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/icons/partner-icon-5.png"
+                  alt=""
+                  src={food6}
+                  data-was-processed="true"
+                />
+              </div>
+              <div class="partners-intro--partners-item -pos-6">
+                <img
+                  class="lazy loaded"
+                  data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/icons/partner-icon-6.png"
+                  alt=""
+                  src={food}
+                  data-was-processed="true"
+                />
+              </div>
+              <div class="partners-intro--partners-item -pos-7">
+                <img
+                  class="lazy loaded"
+                  data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/icons/partner-icon-7.png"
+                  alt=""
+                  src={food7}
+                  data-was-processed="true"
+                />
+              </div>
+              <div class="partners-intro--partners-item -pos-8">
+                <img
+                  class="lazy loaded"
+                  data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/icons/partner-icon-8.png"
+                  alt=""
+                  src={food8}
                   data-was-processed="true"
                 />
               </div>
             </div>
-           
-          </div>
+            <div class="partners-intro--logo">
+              <img
+                class="lazy loaded"
+                data-src="https://fieldedge.com/wp-content/themes/fieldedge/assets/dist/images/logo-vector.svg"
+                alt=""
+                src={food1}
+                data-was-processed="true"
+              />
             </div>
           </div>
-     );
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Foods;
--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,49 +1,53 @@
-import React from 'react';
-import about1 from "../../assets/For_Signle_Image-removebg-preview.png"
+import React from "react";
+import about1 from "../../assets/For_Signle_Image-removebg-preview.png";
 const Team = () => {
-    return (
-        <div>
-            <section class="w-full mx-auto py-10 bg-gradient-to-r from-[#017445] via-[#020629] to-[#EF932A] dark:text-white">
+  return (
+    <div>
+      <section class="w-full mx-auto py-10 bg-gradient-to-r from-[#017445] via-[#020629] to-[#EF932A] dark:text-white">
         {/* <div class="w-fit pb-1 px-2 mx-4 rounded-md text-2xl font-semibold border-b-2 border-blue-600 dark:border-b-2 dark:border-yellow-600">Services</div> */}
         <h2
-            style={{ lineHeight: "120%" }}
-            className="animated slide-text text-[25px] lg:text-[40px] font-bold   text-center   text-white "
-          >
-            {" "}
-        OUR TEAM
-          </h2>
-        <div class="xl:w-[80%] sm:w-[85%] xs:w-[90%] mx-auto sm:flex md:flex lg:flex xs:flex-col lg:gap-4 xs:gap-2 justify-center lg:items-stretch md:items-center mt-4">
-          <div class="lg:w-[50%] xs:w-full">
-            <img
-              class="lg:rounded-t-lg sm:rounded-sm xs:rounded-sm"
-              src={about1}
-              alt="billboard image"
-            />
-          </div>
-          <div class="lg:w-[50%] sm:w-full xs:w-full   dark:text-gray-400 md:p-4 xs:p-0 rounded-md">
-            {/* <h2 class="text-3xl font-semibold text-gray-900 dark:text-white">
-              Lorem ipsum dolor sit amet consectetur
-            </h2> */}
-            <p class="text-2xl mt-4 text-white">
-              Edu Solutions is renowned as a premier consultancy firm in Bangladesh,
-              dedicated to facilitating study opportunities primarily in
-               UK, USA, Canada,Dubai,Newzeland,Romania and Australia. Serving as a comprehensive
-              platform, EduCare BD provides students with information on
-              admissions, scholarships, and financial aid across 160+
-              universities worldwide. Committed to maintaining exceptional
-              quality, Edu Solutions  offers unwavering support to students,
-              recognizing the significance of their future decisions. With a
-              trusted reputation in Bangladesh, Edu Solutions  boasts a dedicated
-              team providing round-the-clock assistance, with  in Dhaka.
-           
-            </p>
-          </div>
-        </div>
-        {/* <!-- col-2 --> */}
+          style={{ lineHeight: "120%" }}
+          className="animated slide-text text-[25px] lg:text-[40px] font-bold   text-center   text-white "
+        >
+          {" "}
+          OUR TEAM
+        </h2>
        
-      </section> 
-        </div>
-    );
+
+        <section class=" ">
+          <div class="max-w-7xl mx-auto   px-4 sm:px-6   lg:px-8">
+           
+            <div class="mt-16 lg:mt-20">
+              <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+                <div class="rounded-lg overflow-hidden">
+                   
+                  <img
+                    class="lg:rounded-t-lg sm:rounded-sm xs:rounded-sm"
+                    src={about1}
+                    alt="billboard image"
+                  />
+                </div>
+                <div>
+                  <div class="max-w-full mx-auto rounded-lg overflow-hidden">
+                    <p class="text-2xl mt-4 text-white">
+                      For over a generation, we've delighted food lovers
+                      nationwide with our exceptional Bangla, Thai,
+                      Chines,Desserts and Indian cuisine. Our mission has always
+                      been to satisfy our customers' palates. Nestled in the
+                      heart of Dhaka, our tranquil garden and serene interiors
+                      offer an unmatched dining experience. Join us for a
+                      memorable meal with your loved ones and savor the moment.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* <!-- col-2 --> */}
+      </section>
+    </div>
+  );
 };
 
 export default Team;
