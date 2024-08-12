c797469bb26a1fa5b47092be973921bbd2ca4380
Sadnan-2019 <sadnan.wow@gmail.com>
2024-08-12 16:46:29 +0600

    fdooitem

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -4,6 +4,11 @@ import reviewone from "../../assets/review1.jpg";
 import reviewtwo from "../../assets/review2.jpg";
 import reviewtthree from "../../assets/review3.jpg";
 import reviewfour from "../../assets/review4.jpg";
+import reviewfive from "../../assets/review5.jpg";
+import review6 from "../../assets/review6.jpg";
+import review7 from "../../assets/review7.png";
+import review8 from "../../assets/review8.jpg";
+import review9 from "../../assets/review9.jpg";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
@@ -115,7 +120,7 @@ const Review = () => {
                   <div class="w-full lg:w-2/3 p-4">
                     <div class="inline-grid">
                       <p class="work-sans font-semibold text-xl text-white">
-                      Nahid Hasan
+                        Nahid Hasan
                       </p>
                       <p class="raleway text-sm my-4 text-white opacity-75">
                         One of best restaurant in Maymanshing .
@@ -142,10 +147,10 @@ const Review = () => {
                   <div class="w-full lg:w-2/3 p-4">
                     <div class="inline-grid">
                       <p class="work-sans font-semibold text-xl text-white">
-                      Biplob Nebil
+                        Biplob Nebil
                       </p>
                       <p class="raleway text-sm my-4 text-white opacity-75">
-                      Really Nice environment, very good food and service
+                        Really Nice environment, very good food and service
 
                       </p>
                     </div>
@@ -153,7 +158,152 @@ const Review = () => {
                 </div>
               </div>
             </SwiperSlide>
-            
+            <SwiperSlide>
+              <div class="container mx-auto py-12 px-4">
+                <div class="inline-grid max-w-xs sm:max-w-xs lg:max-w-lg lg:flex bg-gradient-to-r from-[#017445] via-[#160a2c] to-[#EF932A] rounded-lg border shadow-lg pb-6 lg:pb-0">
+                  <div class="w-full lg:w-1/3 lg:p-4">
+                    <img
+                      src={reviewfive}
+                      alt="image"
+                      class="h-64 lg:h-full object-cover object-center w-full"
+                    />
+                  </div>
+
+                  <div class="w-full lg:w-2/3 p-4">
+                    <div class="inline-grid">
+                      <p class="work-sans font-semibold text-xl text-white">
+                        Zahiruddin Mahmud
+                      </p>
+                      <p class="raleway text-sm my-4 text-white opacity-75">
+                        Food and Staff both were exceptional. Very delicious food and behavior decent. I would highly recommend all to dine at Tulip Garden Restaurant
+
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div class="container mx-auto py-12 px-4">
+                <div class="inline-grid max-w-xs sm:max-w-xs lg:max-w-lg lg:flex bg-gradient-to-r from-[#017445] via-[#160a2c] to-[#EF932A] rounded-lg border shadow-lg pb-6 lg:pb-0">
+                  <div class="w-full lg:w-1/3 lg:p-4">
+                    <img
+                      src={review6}
+                      alt="image"
+                      class="h-64 lg:h-full object-cover object-center w-full"
+                    />
+                  </div>
+
+                  <div class="w-full lg:w-2/3 p-4">
+                    <div class="inline-grid">
+                      <p class="work-sans font-semibold text-xl text-white">
+                        Harun Shikder
+                      </p>
+                      <p class="raleway text-sm my-4 text-white opacity-75">
+                        Very large space and arrangements
+                        for family hangouts, Foods are very delicious & enough portions.
+
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div class="container mx-auto py-12 px-4">
+                <div class="inline-grid max-w-xs sm:max-w-xs lg:max-w-lg lg:flex bg-gradient-to-r from-[#017445] via-[#160a2c] to-[#EF932A] rounded-lg border shadow-lg pb-6 lg:pb-0">
+                  <div class="w-full lg:w-1/3 lg:p-4">
+                    <img
+                      src={review7}
+                      alt="image"
+                      class="h-64 lg:h-full object-cover object-center w-full"
+                    />
+                  </div>
+
+                  <div class="w-full lg:w-2/3 p-4">
+                    <div class="inline-grid">
+                      <p class="work-sans font-semibold text-xl text-white">
+                        আজহারুল হক                      </p>
+                      <p class="raleway text-sm my-4 text-white opacity-75">
+                        They have great interior theme. And
+                        their lood quality is soo good
+                        Chill
+                        out
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div class="container mx-auto py-12 px-4">
+                <div class="inline-grid max-w-xs sm:max-w-xs lg:max-w-lg lg:flex bg-gradient-to-r from-[#017445] via-[#160a2c] to-[#EF932A] rounded-lg border shadow-lg pb-6 lg:pb-0">
+                  <div class="w-full lg:w-1/3 lg:p-4">
+                    <img
+                      src={review8}
+                      alt="image"
+                      class="h-64 lg:h-full object-cover object-center w-full"
+                    />
+                  </div>
+
+                  <div class="w-full lg:w-2/3 p-4">
+                    <div class="inline-grid">
+                      <p class="work-sans font-semibold text-xl text-white">
+                        Abara Zawad Sami                  </p>
+                      <p class="raleway text-sm my-4 text-white opacity-75">
+                        Foods are just yummy. Liked the place and service also, Stylish interior, Fine dining, Healthy options
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div class="container mx-auto py-12 px-4">
+                <div class="inline-grid max-w-xs sm:max-w-xs lg:max-w-lg lg:flex bg-gradient-to-r from-[#017445] via-[#160a2c] to-[#EF932A] rounded-lg border shadow-lg pb-6 lg:pb-0">
+                  <div class="w-full lg:w-1/3 lg:p-4">
+                    <img
+                      src={review9}
+                      alt="image"
+                      class="h-64 lg:h-full object-cover object-center w-full"
+                    />
+                  </div>
+
+                  <div class="w-full lg:w-2/3 p-4">
+                    <div class="inline-grid">
+                      <p class="work-sans font-semibold text-xl text-white">
+                        Sporshomoi Riad                </p>
+                      <p class="raleway text-sm my-4 text-white opacity-75">
+                        Food was really very good! Alhamdulilah satisfied with their service!thanks!                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div class="container mx-auto py-12 px-4">
+                <div class="inline-grid max-w-xs sm:max-w-xs lg:max-w-lg lg:flex bg-gradient-to-r from-[#017445] via-[#160a2c] to-[#EF932A] rounded-lg border shadow-lg pb-6 lg:pb-0">
+                  <div class="w-full lg:w-1/3 lg:p-4">
+                    <img
+                      src={review7}
+                      alt="image"
+                      class="h-64 lg:h-full object-cover object-center w-full"
+                    />
+                  </div>
+
+                  <div class="w-full lg:w-2/3 p-4">
+                    <div class="inline-grid">
+                      <p class="work-sans font-semibold text-xl text-white">
+                      AR Rahman Buiya              </p>
+                      <p class="raleway text-sm my-4 text-white opacity-75">
+                        Alhamdulillah... at last Trishala Valo Kono khabar er restaurant peyesi.. Good qualhty and good quantity.Asha korsi Tulip Garden restaurant tader quality same rakhbe always.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+
           </div>
         </Swiper>
       </div>
